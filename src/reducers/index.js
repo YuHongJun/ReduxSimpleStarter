9e3a651dca51f03a756016873b10baf89a5835f0
余虹君 <yuhongjun@yhjMacBook-Pro.local>
2017-03-02 07:01:29 +0800

    yt

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
-import BooksReducer form './reducer_books';
+
 const rootReducer = combineReducers({
-  // state: (state = {}) => state
-  books:BooksReducer
+  state: (state = {}) => state
 });
 
 export default rootReducer;
